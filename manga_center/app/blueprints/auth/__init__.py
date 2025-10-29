8e589bb281643358f416ff79502dcb269b1df955
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-29 17:00:13 +0530

    Add files via upload

---
+++ manga_center/app/blueprints/auth/__init__.py
@@ -0,0 +1,6 @@
+# app/blueprints/auth/__init__.py
+from flask import Blueprint
+
+auth_bp = Blueprint('auth', __name__, template_folder='../../templates/auth', static_folder='../../static')
+
+from . import routes  # noqa: F401
