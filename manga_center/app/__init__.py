80fa8a2a05c995b4bdeaa565f7e925ca99adaa95
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 18:08:32 +0530

    Add files via upload

--- manga_center/app/__init__.py
+++ manga_center/app/__init__.py
@@ -1,28 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+def create_app():
+    app = Flask(__name__)
 
-def create_app(config: dict = None):
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.update(
-        SECRET_KEY = config.get("SECRET_KEY", "dev") if config else "dev",
-        SQLALCHEMY_DATABASE_URI = config.get("DATABASE_URI", "sqlite:///manga.db") if config else "sqlite:///manga.db",
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
-    )
+    app.config['SECRET_KEY'] = 'ariptmanga123'
 
-    db.init_app(app)
-    login_manager.init_app(app)
+    from app.routes.main import main_bp
+    from app.routes.users import users_bp
 
-    # register blueprints
-    from .routes.main import main_bp
     app.register_blueprint(main_bp)
-
-    # create DB tables if they don't exist
-    with app.app_context():
-        from . import models
-        db.create_all()
+    app.register_blueprint(users_bp)
 
     return app
--- manga_center/app/routes/main.py
+++ manga_center/app/routes/main.py
@@ -1,31 +1,7 @@
-from flask import Blueprint, render_template, url_for, abort
-from ..models import Manga, Chapter
-from .. import db
-import os
+from flask import Blueprint, render_template
 
-main_bp = Blueprint("main", __name__)
+main_bp = Blueprint('main', __name__)
 
-@main_bp.route("/")
-def index():
-    mangas = Manga.query.order_by(Manga.title).all()
-    return render_template("index.html", mangas=mangas)
-
-@main_bp.route("/manga/<slug>/")
-def manga_page(slug):
-    manga = Manga.query.filter_by(slug=slug).first_or_404()
-    chapters = sorted(manga.chapters, key=lambda c: c.number)
-    return render_template("manga.html", manga=manga, chapters=chapters)
-
-@main_bp.route("/manga/<slug>/chapter/<int:chapter_id>/")
-def read_chapter(slug, chapter_id):
-    chapter = Chapter.query.get_or_404(chapter_id)
-    if chapter.manga.slug != slug:
-        abort(404)
-    # list files in static/uploads/<folder>
-    static_folder = os.path.join("app", "static")
-    chapter_dir = os.path.join(static_folder, chapter.folder)
-    if not os.path.isdir(chapter_dir):
-        abort(404)
-    images = sorted([f for f in os.listdir(chapter_dir) if f.lower().endswith((".jpg",".jpeg",".png"))])
-    image_urls = [url_for("static", filename=f"uploads/{chapter.folder}/{img}") for img in images]
-    return render_template("read.html", images=image_urls, chapter=chapter)
+@main_bp.route('/')
+def home():
+    return render_template('index.html')
