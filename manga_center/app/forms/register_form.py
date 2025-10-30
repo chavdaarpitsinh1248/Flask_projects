e1bef4599567e0063e03d9c7626f174f021cce83
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 18:44:12 +0530

    Add files via upload

---
+++ manga_center/app/forms/register_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, PasswordField
+from wtforms.validators import Length, DataRequired, EqualTo, Email
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
