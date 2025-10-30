f509ba1255415a74f65ccf22cc279dd908628f43
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 19:27:25 +0530

    Add files via upload

--- manga_center/app/routes/users.py
+++ manga_center/app/routes/users.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.forms.login_form import LoginForm
 from app.forms.register_form import RegisterForm
+from app.models import User
+from app import db
 
 users_bp = Blueprint('users', __name__)
 
@@ -16,6 +19,23 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
+        # Check if username or email already exist
+        existing_user = User.query.filter(
+            (User.username == form.username.data) | (User.email == form.email.data)
+        ).first()
+
+        if existing_user:
+            flash('Username or Email already Exist!', "danger")
+        else:
+            hashed_password = generate_password_hash(form.passowrd.data)
+            new_user = User(
+                username = form.username.data,
+                email = form.email.data,
+                passowrd = hashed_password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+        flash(f"Account created for {form.username.data}! You can now log in.", "success")
         return redirect(url_for("users.login"))
     return render_template('register.html', form=form)
