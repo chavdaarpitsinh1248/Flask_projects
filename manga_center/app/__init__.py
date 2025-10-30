eb666909d3f7b06608cfe9f2978d589386939737
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 19:53:35 +0530

    Add files via upload

--- manga_center/app/__init__.py
+++ manga_center/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +13,11 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    login_manager.init_app(app)
+
+    #Redirect unauthorized users to login page
+    login_manager.login_view = 'users.login'
+    login_manager.login_message_category = 'info'
 
     from app.routes.main import main_bp
     from app.routes.users import users_bp
