e7558019587a15dad3a9dd3fc524cd7c836b4573
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 19:10:40 +0530

    Add files via upload

--- manga_center/app/__init__.py
+++ manga_center/app/__init__.py
@@ -1,14 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'ariptmanga123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manga_center.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from app.routes.main import main_bp
     from app.routes.users import users_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(users_bp)
+
+    #Import Models to make sure they are register
+    from app import models
+
+    #create database table if not exist
+    with app.app_context():
+        db.create_all()
 
     return app
