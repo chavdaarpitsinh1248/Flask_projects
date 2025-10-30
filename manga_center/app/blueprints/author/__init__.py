6ce75b0735cb97a53a0a76159778a9fd8a8e8c9c
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-30 15:56:25 +0530

    Delete manga_center directory

--- manga_center/app/blueprints/author/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint
-
-# Point the author blueprint to the 'templates/manga' folder.
-# Author-specific templates will live under templates/manga/author/
-author_bp = Blueprint('author', __name__, template_folder='../../templates/manga', static_folder='../../static')
-
-from . import routes  # noqa: F401
