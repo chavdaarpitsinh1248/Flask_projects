d70cfea7a541796507979b85927536d75afad3a8
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-31 19:31:47 +0530

    Add files via upload

--- manga_center/app/routes/author.py
+++ manga_center/app/routes/author.py
@@ -1,4 +1,8 @@
-from flask import flash, Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from app.forms.manga_forms import MangaForm
+from app.models import Manga
+from app import db
+import os
 from flask_login import login_required, current_user
 
 author_bp = Blueprint('author', __name__, url_prefix='/author')
@@ -15,3 +19,28 @@ def restrict_to_authors():
 @author_bp.route('/')
 def dashboard():
     return render_template('author/dashboard.html')
+
+# Add Manga
+@author_bp.route('/add_manga', methods=['GET', 'POST'])
+def add_manga():
+    form = MangaForm()
+
+    if form.validate_on_submit():
+        title = form.title.data,
+        description = form.description.data,
+        cover_image = None  # We'll add upload support later
+
+        new_manga = Manga(
+            title = title,
+            description = description,
+            cover_image = cover_image,
+            author_id = current_user.author_profile.id
+        )
+
+        db.session.add(new_manga)
+        db.session.commit()
+
+        flash(f"Manga {title} Added Successfully!", "success")
+        return redirect(url_for('author.dashboard'))
+    
+    return render_template('author/add_manga.html', form=form)
