7ddc15f36f2e0aaf5fda1877d2992a87369c21ba
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-10-31 17:49:53 +0530

    Delete manga_reader directory

--- manga_reader/manga_center/routes/user_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import login_required, current_user
-from datetime import datetime
-from .. import db
-from ..models import StudioRequest, Notification, User
-
-user_bp = Blueprint('user', __name__)
-
