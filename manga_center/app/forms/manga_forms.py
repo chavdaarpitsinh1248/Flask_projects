78b3f4ce71d4e984e311a37ec8c78db85d9c4b46
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-18 18:01:13 +0530

    Add files via upload

--- manga_center/app/forms/manga_forms.py
+++ manga_center/app/forms/manga_forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FileField, SubmitField, IntegerField, MultipleFileField
+from wtforms import StringField, TextAreaField, FileField, SubmitField, IntegerField, MultipleFileField, SelectMultipleField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 class MangaForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=5, max=150)])
     description = TextAreaField('Description', validators=[Length(max=1000)])
     cover_image = FileField('Cover Image (optional)')
+    genres = SelectMultipleField('Genres', coerce=int, choices=[])
     submit = SubmitField('Add Manga')
 
 class ChapterForm(FlaskForm):
