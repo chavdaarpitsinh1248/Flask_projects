7ee86194ac47092073c084679ec9c5e861d2c3ad
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-19 17:33:47 +0530

    Add files via upload

--- expense_tracker/config.py
+++ expense_tracker/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") 
-    SQLALCHEMY_DATABASE_URI = "sqllite:///expense_tracker.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///expense_tracker.db"
     SQLALCHEMY_TRACK_MODIFICATION = False 
