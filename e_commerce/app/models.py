7f83a3896ece5b9457842e619483782712729b15
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-20 19:07:59 +0530

    Add files via upload

--- e_commerce/app/models.py
+++ e_commerce/app/models.py
@@ -4,3 +4,15 @@ from flask_login import UserMixin
 from sqlalchemy.orm import relationship
 from .extensions import db
 from decimal import Decimal 
+
+
+
+# ---------------------------------------------------
+# ROLE ENUM
+# ---------------------------------------------------
+class RoleEnum(str, Enum):
+    ADMIN = "admin"
+    CUSTOMER = "customer"
+    SUPPILER = "supplier"
+    IN_STAFF = "in_staff"       # warehouse guy
+    DRIVER = "driver"           # delivery guy
