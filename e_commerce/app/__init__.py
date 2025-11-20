8b2c24c53ebc26d28e937f5c1000f7446580c4ac
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-20 18:58:00 +0530

    Add files via upload

---
+++ e_commerce/app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask 
+from .extensions import db, migrate, login_manager
+from .models import User
+from Config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # initialize extensionns
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    # user loader for Flask-Login
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    
+    # register bluprints here
+
+
+    return app
