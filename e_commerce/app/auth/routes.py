f0c83bbfdc8b6d65de6bf4953a77efa32fe7999f
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-21 16:55:13 +0530

    Add files via upload

---
+++ e_commerce/app/auth/routes.py
@@ -0,0 +1,66 @@
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from extensions import db
+from app.models import User
+from . import auth_bp
+from .forms import RegisterForm, LoginForm
+
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # Check if email exists
+        existing = User.query.filter_by(email=form.email.data).first()
+        if existing:
+            flash('Email already exists. Try logging in.', 'danger')
+            #return redirect(url_for('auth.login'))
+        
+        # Create user
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password_hash=generate_password_hash(form.password.data),
+            role='customer'
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        flask('Account created! You can now log in.', 'success')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user)
+
+            next_page = request.args.get('next'
+            return redirect(next_page) if next_page else redirect(url_for('main.home')))
+
+        flash('Invalid email or password', 'danger')
+    
+    return render_template('auth/login.html', form=form)
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully.', 'info')
+    return redirect(url_for('auth.login'))
