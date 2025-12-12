c12c32a25d7126b161fa0c0fb03fd1a672d7e7b7
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-12 20:01:59 +0530

    Delete e_commerce directory

--- e_commerce/app/supplier/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-supplier_bp = Blueprint("supplier", __name__, url_prefix="/supplier")
-
-from . import routes
--- e_commerce/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-1234")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///ecommerce.db")
-    SQLALCHEMY_TRAACK_MODIFICATIONS = False
